44d1eb3dc51b7ce4546d8070793357538ff52001
우영 <wooyoung@uyeong-ui-MacBook-Pro.local>
2018-12-27 09:49:29 +0900

    misson complete

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private String userId;
     private String password;
@@ -32,5 +35,24 @@ public class User {
     @Override
     public String toString() {
         return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+    }
+    
+    public static boolean equal(User user1, User user2) {
+    	if(user1.userId == user2.userId && user1.password == user2.password && user1.name == user2.name && user1.email == user2.email) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
+    
+	public static List<User> getUserList(){
+    	List<User> users = new ArrayList<>();
+    	
+    	users.add(new User("test1","1","테스트1","test1@test.com"));
+    	users.add(new User("test2","2","테스트2","test2@test.com"));
+    	users.add(new User("test3","3","테스트3","test3@test.com"));
+    	
+    	return users;
     }
 }
